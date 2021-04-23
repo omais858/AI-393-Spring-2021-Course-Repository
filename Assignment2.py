7f5583bcbaa0f2af7f7953d9b70109e17cf5f1a1
fasihrohan <77568715+fasihrohan@users.noreply.github.com>
2021-04-23 09:14:24 +0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -183,3 +183,20 @@ def selectParametersForKNN(a, b, c, d):
 
 
 selectParametersForKNN(X_train, Y_train, X_test, Y_test);
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+  
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
